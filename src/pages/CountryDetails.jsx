bc824500bd9095a801d8a8d1dbc7b60ecc107d3f
dylan.silveira <dylan.silveira@edu.pucrs.br>
2022-04-29 20:49:35 -0300

    Add photos carousel

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Center, Container, Heading } from '@chakra-ui/react'
 import DescriptionBox from '../components/DescriptionBox'
 import CountryImage from '../components/CountryImage'
+import PhotosCarousel from '../components/PhotosCarousel'
 import StartJourneyButton from '../components/buttons/StartJouneyButton'
 
 export default function CountryDetails (props) {
@@ -10,7 +11,12 @@ export default function CountryDetails (props) {
   const country = {
     name: 'Canada',
     image: 'https://olhardigital.com.br/wp-content/uploads/2021/05/Canada.jpg',
-    description: 'Breve descrição do país'
+    description: 'Breve descrição do país',
+    photos: [
+      'https://www.ie.com.br/wp-content/uploads/2021/12/cidades-mais-quentes-canada.jpeg',
+      'https://www.ie.com.br/wp-content/uploads/2022/01/onde-fica-canada.jpeg',
+      'https://www.tirarvistocanadense.com.br/wp-content/uploads/2020/01/Canad%C3%A1-min.jpeg'
+    ]
   }
 
   return (
@@ -29,6 +35,7 @@ export default function CountryDetails (props) {
         <DescriptionBox text={country.description} />
         <StartJourneyButton />
       </Container>
+      <PhotosCarousel photos={country.photos}/>
     </div>
   )
 }
