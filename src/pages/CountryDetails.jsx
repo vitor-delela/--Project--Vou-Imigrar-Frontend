65fdf28a149e1904f449b5d1eacbf0ae0498a236
dylan.silveira <dylan.silveira@edu.pucrs.br>
2022-04-29 21:57:43 -0300

    Add country information

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -3,6 +3,7 @@ import { Center, Container, Heading } from '@chakra-ui/react'
 import DescriptionBox from '../components/DescriptionBox'
 import CountryImage from '../components/CountryImage'
 import PhotosCarousel from '../components/PhotosCarousel'
+import CountryInformation from '../components/CountryInformation'
 import StartJourneyButton from '../components/buttons/StartJouneyButton'
 
 export default function CountryDetails (props) {
@@ -16,7 +17,17 @@ export default function CountryDetails (props) {
       'https://www.ie.com.br/wp-content/uploads/2021/12/cidades-mais-quentes-canada.jpeg',
       'https://www.ie.com.br/wp-content/uploads/2022/01/onde-fica-canada.jpeg',
       'https://www.tirarvistocanadense.com.br/wp-content/uploads/2020/01/Canad%C3%A1-min.jpeg'
-    ]
+    ],
+    information: {
+      clima: 'Entre julho e setembro, os meses mais quentes, a temperatura média é de 25°C. No inverno, fica entre 10°C e 19°C',
+      idioma: 'Inglês',
+      populacao: 'Cerca de 36 milhões de habitantes',
+      territorio: '9.985.000 km²',
+      timezone: '(UTC -7 a UTC -2:30) 4 horas a menos à meia hora à frente do horário do Brasil',
+      moeda: 'Dólar Canadense',
+      ddi: '+1',
+      voltagem: '110 V'
+    }
   }
 
   return (
@@ -36,6 +47,9 @@ export default function CountryDetails (props) {
         <StartJourneyButton />
       </Container>
       <PhotosCarousel photos={country.photos}/>
+      <CountryInformation information={country.information}/>
+      <StartJourneyButton />
+      <Container paddingBottom='80px'/>
     </div>
   )
 }
