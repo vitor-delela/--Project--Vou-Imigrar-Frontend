1def9b72ec6a50de311850a561fd66f36a6e4f6c
Matheu Góes <matheuge@gmail.com>
2022-05-03 17:57:06 -0300

    add slice persist in project

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,26 @@ import { useDispatch } from 'react-redux'
 import pageReducer from './pageSlice'
 import userReducer from './userSlice'
 
-const store = configureStore({
-  reducer: {
-    page: pageReducer,
-    user: userReducer
-  }
-})
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+import thunk from 'redux-thunk';
 
-export const useAppDispatch = () => useDispatch()
+const reducers = combineReducers({
+  page: pageReducer,
+  user: userReducer
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
+});
+
 export default store
