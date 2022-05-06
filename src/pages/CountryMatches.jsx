41918253e4822890350e3ebdf9f2fd314d0cc1c6
nicolas marques <nicolasmarques1205@gmail.com>
2022-05-06 15:15:56 -0300

    changes to the countryCard component and made the country card clickable

--- src/pages/CountryMatches.jsx
+++ src/pages/CountryMatches.jsx
@@ -16,12 +16,24 @@ export default function CountryMatches () {
     dispatch(setPage('Match de Países'))
   })
 
+  
   return (
     <Box marginTop="20px">
-      <CountryCard
-        src= "https://olhardigital.com.br/wp-content/uploads/2021/05/Canada.jpg"
-        name= 'Canadá'
-      />
+      <div id="my-div">
+        <a href='/home' class="fill-div">
+          <CountryCard
+          src= "https://www.estudarfora.org.br/wp-content/uploads/2021/09/Estados-Unidos-768x538.jpg"
+          name= 'Estados Unidos'
+          percentage= '90%'
+          />
+        </a>
+      </div>
+      
+      <Box w='100%' p={6}>
+        <PrimaryButton borderRadius={8} onClick={() => ""}>
+          Ver todos os países 
+        </PrimaryButton>
+      </Box>
     </Box>
   )
 }
