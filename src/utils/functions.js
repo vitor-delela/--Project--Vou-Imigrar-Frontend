79e7104af9e64928474e3d90847d66f94af9922d
André Marques <andre.marques@edu.pucrs.br>
2022-05-12 02:56:39 -0300

    fix regex verifyName

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -4,12 +4,12 @@ export function verifyEmail (email) {
     .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 }
 
-export function verifyName(name){
-  let re = new RegExp("^[a-zA-Z]+ [a-zA-Z]")
+export function verifyName(name) {
+  let re = new RegExp("^[A-zÀ-ÿ]+ [A-zÀ-ÿ]")
   return re.test(name)
 }
 
-export function verifyPassword(password){
+export function verifyPassword(password) {
   return password.length>=8
           && password
           .match(/^(?=(?:.*?[A-Z]){1})(?=(?:.*?[a-z]){1})(?=(?:.*?[0-9]){1})/)
