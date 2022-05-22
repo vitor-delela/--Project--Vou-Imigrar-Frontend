ecf4dd8996129c054ae30dbf4d8caab13cff372b
Guilherme Luz <guilhermeluzds@gmail.com>
2022-05-22 16:08:35 -0300

    fixed imports and minor fix on StartJourneyButton

--- src/components/buttons/StartJouneyButton.jsx
+++ src/components/buttons/StartJouneyButton.jsx
@@ -3,16 +3,19 @@ import { Icon } from '@chakra-ui/react'
 import PrimaryButton from './PrimaryButton'
 import { MdOutlineAirplanemodeActive } from 'react-icons/md'
 import { useNavigate } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
 import { startJourney } from '../../store/journeySlice'
 
+export default function StartJourneyButton (props) {
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
-  export default function StartJourneyButton (props) {
-    const navigate = useNavigate()
-
-    const beginJourney = async () => {
-      dispatch(startJourney(props.countryId))
-      navigate(`/country/${props.countryId}`) 
-    }
+  const beginJourney = async () => {
+    console.log(props)
+    dispatch(startJourney(props)).then(() => {
+      navigate(`/journey/${props.countryId}`)
+    })
+  }
   return (
     <PrimaryButton
       height='50px'
@@ -20,14 +23,13 @@ import { startJourney } from '../../store/journeySlice'
       fontSize='18'
       fontWeight='bold'
       marginTop='20px'
-      rightIcon={<Icon marginLeft='5px' fontSize='20' as={MdOutlineAirplanemodeActive}/>}
+      rightIcon={<Icon marginLeft='5px' fontSize='20' as={MdOutlineAirplanemodeActive} />}
       onClick={beginJourney}
 
       {...props}
     >
       Iniciar jornada
     </PrimaryButton>
-  
 
   )
 }
