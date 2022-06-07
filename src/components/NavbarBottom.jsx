6374c49ae5c7ed698389c20d8670e58c92f82725
Dylan Silveira <dylan.silveira@edu.pucrs.br>
2022-06-06 21:17:26 -0300

    Change bottom menu to highlight selected item

--- src/components/NavbarBottom.jsx
+++ src/components/NavbarBottom.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
+import { SlideFade } from '@chakra-ui/react'
 
 import { BiWorld } from 'react-icons/bi'
 import { MdHome, MdPerson } from 'react-icons/md'
-import { Button, Flex } from '@chakra-ui/react'
+import { Button, Flex, Stack, Box } from '@chakra-ui/react'
 
 export default function NavbarBottom () {
   const navigate = useNavigate()
+  const location = useLocation()
+  const selectedLine = <SlideFade in={true}><Box width={6} height='2px' bg='white' /></SlideFade>
 
   return (
     <Flex
@@ -21,14 +24,32 @@ export default function NavbarBottom () {
       alignItems='top'
       bg='purple'
     >
-      <Button background='transparent' onClick={() => { navigate('/country-matches') }}>
-        <BiWorld color='white' fontSize={32} />
+      <Button bg='transparent' color='white' fontSize={32} onClick={() => { navigate('/country-matches') }} >
+        {
+          location.pathname == '/country-matches'
+          ? <Stack spacing='5px' align='center'>
+              <BiWorld fontSize={36} /> {selectedLine}
+            </Stack>
+          : <BiWorld opacity={0.7} />
+        }
       </Button>
-      <Button background='transparent' onClick={() => { navigate('/home') }}>
-        <MdHome color='white' fontSize={32} />
+      <Button bg='transparent' color='white' fontSize={32} onClick={() => { navigate('/home') }}>
+        {
+          location.pathname == '/home'
+          ? <Stack spacing='5px' align='center'>
+              <MdHome fontSize={36} /> {selectedLine}
+            </Stack>
+          : <MdHome opacity={0.7} />
+        }
       </Button>
-      <Button background='transparent' onClick={() => { navigate('/profile') }}>
-        <MdPerson color='white' fontSize={32} />
+      <Button bg='transparent' color='white' fontSize={32} onClick={() => { navigate('/profile') }}>
+        {
+          location.pathname == '/profile'
+          ? <Stack spacing='5px' align='center'>
+              <MdPerson fontSize={36} /> {selectedLine}
+            </Stack>
+          : <MdPerson opacity={0.7} />
+        }
       </Button>
     </Flex>
   )
