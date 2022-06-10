838306f173f89fecd636a63cf82330d7b3ca5dbf
Dylan Silveira <dylan.silveira@edu.pucrs.br>
2022-06-10 20:27:16 -0300

    Change journey link
    Co-authored-by: Nicolas Kics <nicolas.kics@edu.pucrs.br>

--- src/pages/CountryDetails.jsx
+++ src/pages/CountryDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { setPage } from '../store/pageSlice'
-import { Container, Box, useToast, Spinner, Center, Text } from '@chakra-ui/react'
+import { Container, Box, useToast, Spinner, Center, Text, Stack, Link } from '@chakra-ui/react'
 import DescriptionBox from '../components/DescriptionBox'
 import CountryImage from '../components/CountryImage'
 import PhotosCarousel from '../components/PhotosCarousel'
@@ -9,9 +9,8 @@ import CountryInformation from '../components/CountryInformation'
 import StartJourneyButton from '../components/buttons/StartJourneyButton'
 import { getCountryDetails } from '../store/countrySlice'
 import { useNavigate, useParams } from 'react-router-dom'
-import PrimaryButton from '../components/buttons/PrimaryButton'
 
-export default function CountryDetails (props) {
+export default function CountryDetails(props) {
   const { id } = useParams()
   const toast = useToast()
   const navigate = useNavigate()
@@ -54,15 +53,23 @@ export default function CountryDetails (props) {
           {country.hasStartedJourney === 'N'
             ? <StartJourneyButton country={country.id} />
             : (
-              <Text fontSize="14" fontWeight="extrabold" mt="2" textAlign="center">
-                Você já possui jornada para o país!
-                <Box mt="2">
-                  <PrimaryButton w='90%' onClick={() => navigate('/journey/${country.id}')}>
-                    Ir para a jornada iniciada 
-                  </PrimaryButton>
-                </Box>
-              </Text>
-              )
+              <Stack alignItems='center' spacing={0.5}>
+                <Text fontSize="14" fontWeight="extrabold" mt="2">
+                  Você já possui jornada para o país!
+                </Text>
+                <Link
+                  onClick={() => { navigate(`/journey/${country.id}`) }}
+                  display='flex'
+                  alignItems='center'
+                  color='purple'
+                  isExternal
+                  fontSize={14}
+                  style={{ textDecorationLine: 'underline' }}
+                >
+                  Ir para a jornada iniciada
+                </Link>
+              </Stack>
+            )
           }
         </Container>
         <PhotosCarousel photos={country.photos} />
@@ -73,7 +80,7 @@ export default function CountryDetails (props) {
             : null
         }
       </Box>
-      )
+    )
     : (
       <Center w='100%' maxW='600px' mt={8} mb={16}>
         <Spinner
@@ -84,5 +91,5 @@ export default function CountryDetails (props) {
           size='xl'
         />
       </Center>
-      )
+    )
 }
