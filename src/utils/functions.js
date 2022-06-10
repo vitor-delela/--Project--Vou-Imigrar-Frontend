50fdef0969f0066c73798d73d9d36cce4bc145a6
Matheu Góes <matheuge@gmail.com>
2022-06-10 11:47:06 -0300

    lint jsArchives

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -4,13 +4,14 @@ export function verifyEmail (email) {
     .match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
 }
 
-export function verifyName(name) {
-  let re = new RegExp("^[A-zÀ-ÿ]+ [A-zÀ-ÿ]")
+export function verifyName (name) {
+  // eslint-disable-next-line prefer-regex-literals
+  const re = new RegExp('^[A-zÀ-ÿ]+ [A-zÀ-ÿ]')
   return re.test(name)
 }
 
-export function verifyPassword(password) {
-  return password.length>=8
-          && password
-          .match(/^(?=(?:.*?[A-Z]){1})(?=(?:.*?[a-z]){1})(?=(?:.*?[0-9]){1})/)
+export function verifyPassword (password) {
+  return password.length >= 8 &&
+          password
+            .match(/^(?=(?:.*?[A-Z]){1})(?=(?:.*?[a-z]){1})(?=(?:.*?[0-9]){1})/)
 }
